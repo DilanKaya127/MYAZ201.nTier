6913286b83458571798bd21323f452a3a154cb86
Dilan Kaya <201118040@samsun.edu.tr>
2021-12-19 16:59:22 +0300

    Add project files.

---
+++ MAYZ201.nTier.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MAYZ201.nTier.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
